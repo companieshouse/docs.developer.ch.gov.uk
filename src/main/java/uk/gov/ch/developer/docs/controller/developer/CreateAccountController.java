c5337e63ce1f1c54ae71cbe62b5b9635679d8433
ch-dclarke <dclarke@companieshouse.gov.uk>
2019-12-09 09:15:07 +0000

    Abstracted most of the page controller logic

--- src/main/java/uk/gov/ch/developer/docs/controller/developer/CreateAccountController.java
+++ src/main/java/uk/gov/ch/developer/docs/controller/developer/CreateAccountController.java
@@ -2,31 +2,24 @@ package uk.gov.ch.developer.docs.controller.developer;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import uk.gov.ch.developer.docs.controller.BaseController;
+import uk.gov.ch.developer.docs.controller.AbstractPageController;
 
 @Controller
 @RequestMapping("${createAccount.url}")
-public class CreateAccountController extends BaseController {
+public class CreateAccountController extends AbstractPageController {
 
-    private static final String TITLE = "How to Set Up a Companies House Account";
+    private static final String TITLE = "How to Create a Companies House Account";
+
     @Value("${createAccount.path}")
     private String path;
 
-    @GetMapping
-    public String get() {
-        return getTemplateName();
+    public CreateAccountController() {
+        super(TITLE);
     }
 
     @Override
-    protected String getTemplateName() {
+    public String getPath() {
         return path;
     }
-
-    @Override
-    public String getTemplateTitle() {
-        return TITLE;
-    }
-
 }
