a703d02861b40013342cd10849d8a67564f90e9b
Zain-Abbas1 <zabbas@companieshouse.gov.uk>
2019-12-24 11:33:07 +0000

    Create initial implementation of Application Overview page
    Create simple html page, add controller, add unit tests, modify properties file

---
+++ src/main/java/uk/gov/ch/developer/docs/controller/developer/ApplicationOverviewController.java
@@ -0,0 +1,26 @@
+package uk.gov.ch.developer.docs.controller.developer;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import uk.gov.ch.developer.docs.controller.AbstractPageController;
+
+@Controller
+@RequestMapping("${manageApplications.url}")
+public class ApplicationOverviewController extends AbstractPageController {
+    
+    private static final String TITLE = "Application Overview";
+    
+    @Value("${manageApplications.path}")
+    private String path;
+
+    public ApplicationOverviewController() {
+        super(TITLE);
+    }
+
+    @Override
+    public String getPath() {
+        return path;
+    }
+
+}
---
+++ src/test/java/uk/gov/ch/developer/docs/controller/developer/ApplicationOverviewControllerTest.java
@@ -0,0 +1,53 @@
+package uk.gov.ch.developer.docs.controller.developer;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import uk.gov.ch.developer.docs.ApplicationVariables;
+import uk.gov.ch.developer.docs.DocsWebApplication;
+import uk.gov.ch.developer.docs.utility.TestUtils;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DocsWebApplication.class)
+class ApplicationOverviewControllerTest {
+
+    private static final String PATH = "/manage-applications";
+    private static final String VIEW = "dev-hub/manageApplications";
+
+    private MockMvc mockMvc;
+
+    static {
+        TestUtils.setUpEnviromentProperties();
+    }
+
+    @Autowired
+    private WebApplicationContext context;
+    @InjectMocks
+    private ApplicationOverviewController controller;
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    void TestGetRequestReturnsSuccessForCorrectPath() throws Exception {
+        this.mockMvc.perform(get(PATH)).andExpect(status().isOk()).andExpect(view().name(VIEW));
+    }
+    
+    @Test
+    void TestGetRequestReturnsErrorForIncorrectPath() throws Exception {
+        this.mockMvc.perform(get(ApplicationVariables.BADREQUEST_PATH)).andExpect(status().isNotFound());
+    }
+    
+}
