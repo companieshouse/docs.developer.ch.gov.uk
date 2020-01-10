5464ca6c7625aa14b11534c653da689b0874eb52
ch-rfishwick <rfishwick@companieshouse.gov.uk>
2020-01-10 11:09:31 +0000

    Removed redundant Security service and unit test

--- src/main/java/uk/gov/ch/developer/docs/security/WebSecurity.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.ch.developer.docs.security;
-
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-@EnableWebSecurity
-public class WebSecurity {
-
-}
