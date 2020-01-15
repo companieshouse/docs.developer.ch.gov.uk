494af3ee6f86e1edb43423804295d4fdb9439f64
Zain-Abbas1 <zabbas@companieshouse.gov.uk>
2020-01-15 11:32:04 +0000

    initial implementation including sign-in page & correct config to cater for sub domain

---
+++ src/main/java/uk/gov/ch/developer/docs/security/WebSecurity.java
@@ -0,0 +1,36 @@
+package uk.gov.ch.developer.docs.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import uk.gov.companieshouse.auth.filter.HijackFilter;
+import uk.gov.companieshouse.auth.filter.UserAuthFilter;
+import uk.gov.companieshouse.session.handler.SessionHandler;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Configuration
+    @Order(1)
+    public static class TemporaryStartPageSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.antMatcher("/signin")
+                    .addFilterBefore(new SessionHandler(), BasicAuthenticationFilter.class)
+                    .addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class)
+                    .addFilterBefore(new UserAuthFilter(), BasicAuthenticationFilter.class);
+        }
+    }
+
+    @Configuration
+    @Order(2)
+    public static class DocsWebSecurityFilterConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.antMatcher("/**");
+        }
+    }
+}
