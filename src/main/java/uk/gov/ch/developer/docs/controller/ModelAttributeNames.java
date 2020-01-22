5de3dd4706780c26e96169642343a06f5fd47a63
ch-rfishwick <rfishwick@companieshouse.gov.uk>
2020-01-22 13:24:12 +0000

    Finalised ModelAttributeNames

--- src/main/java/uk/gov/ch/developer/docs/controller/ModelAttributeNames.java
+++ src/main/java/uk/gov/ch/developer/docs/controller/ModelAttributeNames.java
@@ -1,7 +1,9 @@
 package uk.gov.ch.developer.docs.controller;
 
-public class ModelAttributeNames {
+public final class ModelAttributeNames {
 
     public static final String USER_MODEL = "userModel";
     public static final String NAV_BAR_MODEL = "navBarModel";
+
+     private ModelAttributeNames() {}
 }
