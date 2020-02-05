8de825965c6ec157de4fe72c88689b0ff782af0f
Zain-Abbas1 <zabbas@companieshouse.gov.uk>
2020-02-05 11:44:29 +0000

    Amend websecurity class & remove commented out lines from controller

--- src/main/java/uk/gov/ch/developer/docs/controller/developer/UserCallbackController.java
+++ src/main/java/uk/gov/ch/developer/docs/controller/developer/UserCallbackController.java
@@ -1,10 +1,6 @@
 package uk.gov.ch.developer.docs.controller.developer;
 
 import java.text.ParseException;
-import java.util.Map;
-import java.util.Optional;
-import javax.servlet.ServletRequest;
-import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,84 +15,46 @@ import com.nimbusds.jose.Payload;
 import com.nimbusds.jose.crypto.DirectDecrypter;
 import net.minidev.json.JSONObject;
 import net.minidev.json.JSONValue;
-import uk.gov.ch.developer.docs.controller.AbstractPageController;
-import uk.gov.ch.developer.docs.controller.BaseController;
 import uk.gov.ch.developer.docs.session.SessionService;
 import uk.gov.companieshouse.environment.EnvironmentReader;
 import uk.gov.companieshouse.environment.impl.EnvironmentReaderImpl;
-import uk.gov.companieshouse.session.Harness;
-import uk.gov.companieshouse.session.Session;
-import uk.gov.companieshouse.session.SessionKeys;
-import uk.gov.companieshouse.session.handler.SessionHandler;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
 
 @Controller
 @RequestMapping("${callback.url}")
-public class UserCallbackController extends BaseController{
-    
-UserCallbackController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+public class UserCallbackController {
 
-    //    static String title = "Hello";
-//    
-//    String path = "dev-hub/gettingStarted";
-//    
-//    public UserCallbackController() {
-//        super(title);
-//    }
-//
-//    @Override
-//    public String getPath() {
-//        return path;
-//    }
+    protected Logger LOGGER = LoggerFactory.getLogger("docs.developer.ch.gov.uk");
+
     @Autowired
     private SessionService sessionService;
-    
+
     private static final EnvironmentReader reader = new EnvironmentReaderImpl();
-    
+
     private static final String base64Key = reader.getMandatoryString("OAUTH2_REQUEST_KEY");
-    
+
     @GetMapping
     @ResponseBody
-    public String getParams(@RequestParam("state") String state, @RequestParam("code") String code) throws ParseException, KeyLengthException, JOSEException, net.minidev.json.parser.ParseException {
-//        LOGGER.info("State: " + state.orElseGet(() -> "Not available!!!"));
+    public String getParams(@RequestParam("state") String state, @RequestParam("code") String code)
+            throws ParseException, KeyLengthException, JOSEException,
+            net.minidev.json.parser.ParseException {
         LOGGER.info("State: " + state);
         LOGGER.info("Code: " + code);
 
+        sessionService.getSessionDataFromContext();
+
         JWEObject jweObject = JWEObject.parse(state);
-        
+
         byte[] key = Base64.decodeBase64(base64Key);
         jweObject.decrypt(new DirectDecrypter(key));
-        
+
         Payload payload = jweObject.getPayload();
 
         JSONObject json = (JSONObject) JSONValue.parseWithException(payload.toString());
         String nonce = (String) json.get("nonce");
-        
-//        request.getSession(true);
-//        Session chSession = (Session) request.getAttribute(SessionHandler.CHS_SESSION_REQUEST_ATT_KEY);
-        LOGGER.info("Service: "+ sessionService.getSessionDataFromContext().keySet());
-//        Optional<Map<String,Object>> session = Optional.ofNullable(sessionService.getSessionDataFromContext());
-        
-//        session.ifPresent(d -> LOGGER.info("Session is Available!!!!!"));
-        
-        
-        
-        
+
         return "Found";
-    }
-
-    @Override
-    protected String getTemplateName() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public String getTemplateTitle() {
-        // TODO Auto-generated method stub
-        return null;
     }
 
 }
--- src/main/java/uk/gov/ch/developer/docs/security/WebSecurity.java
+++ src/main/java/uk/gov/ch/developer/docs/security/WebSecurity.java
@@ -5,10 +5,6 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import uk.gov.companieshouse.auth.filter.HijackFilter;
-import uk.gov.companieshouse.auth.filter.UserAuthFilter;
-import uk.gov.companieshouse.session.handler.SessionHandler;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
@@ -19,26 +15,11 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http.antMatcher("/signin");
-//                    .addFilterBefore(new SessionHandler(), BasicAuthenticationFilter.class)
-//                    .addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class)
-//                    .addFilterBefore(new UserAuthFilter(), BasicAuthenticationFilter.class);
-        }
-    }
-    
-    @Configuration
-    @Order(2)
-    public static class GettingStartedSecurityConfig extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.antMatcher("/getting-started")
-                    .addFilterBefore(new SessionHandler(), BasicAuthenticationFilter.class)
-                    .addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class)
-                    .addFilterBefore(new UserAuthFilter(), BasicAuthenticationFilter.class);
         }
     }
 
     @Configuration
-    @Order(3)
+    @Order(2)
     public static class DocsWebSecurityFilterConfig extends WebSecurityConfigurerAdapter {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
