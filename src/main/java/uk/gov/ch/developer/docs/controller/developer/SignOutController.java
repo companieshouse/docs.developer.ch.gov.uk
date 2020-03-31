b6c8f70fff69de5be6043449e044e28e6adef495
Zain-Abbas1 <zabbas@companieshouse.gov.uk>
2020-03-31 09:28:19 +0100

    Make vars final & add path to getmapping

--- src/main/java/uk/gov/ch/developer/docs/controller/developer/SignOutController.java
+++ src/main/java/uk/gov/ch/developer/docs/controller/developer/SignOutController.java
@@ -1,29 +1,26 @@
 package uk.gov.ch.developer.docs.controller.developer;
 
 import java.io.IOException;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import uk.gov.ch.developer.docs.DocsWebApplication;
 import uk.gov.companieshouse.environment.EnvironmentReader;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
 
 @Controller
-@RequestMapping("${signout.url}")
 public class SignOutController {
-    private static final Logger LOGGER = LoggerFactory.getLogger("docs.developer.ch.gov.uk");
+    private static final Logger LOGGER = LoggerFactory.getLogger(DocsWebApplication.APPLICATION_NAME_SPACE);
 
     private final String signOutUrl;
 
-    public SignOutController(EnvironmentReader reader) {
+    public SignOutController(final EnvironmentReader reader) {
         this.signOutUrl = reader.getMandatoryString("CHS_SIGNOUT_URL");
     }
 
-    @GetMapping
-    public void doSignOut(HttpServletRequest httpServletRequest,
-            HttpServletResponse httpServletResponse) throws IOException {
+    @GetMapping("${signout.url}")
+    public void doSignOut(final HttpServletResponse httpServletResponse) throws IOException {
 
         LOGGER.debug("Processing redirect to sign out");
 
