e5ae975f6608050936c4286a3710496f37c4e400
ch-rfishwick <rfishwick@companieshouse.gov.uk>
2020-04-14 14:22:37 +0100

    Reformatted code in sign in test file

--- src/test/java/uk/gov/ch/developer/docs/controller/developer/SignInControllerTest.java
+++ src/test/java/uk/gov/ch/developer/docs/controller/developer/SignInControllerTest.java
@@ -24,6 +24,7 @@ import uk.gov.companieshouse.session.handler.SessionHandler;
 @ExtendWith(MockitoExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SignInControllerTest {
+
     @Mock
     HttpServletRequest request;
     @Mock
@@ -34,21 +35,20 @@ public class SignInControllerTest {
     IIdentityProvider identityProvider;
     @Mock
     Session session;
-
-    @InjectMocks
-    private SignInController signInController;
     StringBuffer requestUrlStringBuffer = new StringBuffer("http://requesturl");
     String originalRequestUri = "originalRequestUri";
     String scope = "scope";
     String nonce = "nonce";
     String email = "email@email";
-
+    @InjectMocks
+    private SignInController signInController;
 
     @Test
     void getSignInTest() throws IOException {
         when(request.getAttribute(SessionHandler.CHS_SESSION_REQUEST_ATT_KEY)).thenReturn(session);
         when(request.getRequestURL()).thenReturn(requestUrlStringBuffer);
-        when(oauth.oauth2EncodeState(any(String.class), any(String.class), any(String.class))).thenReturn("state");
+        when(oauth.oauth2EncodeState(any(String.class), any(String.class), any(String.class)))
+                .thenReturn("state");
         when(identityProvider.getAuthorisationUrl("state")).thenReturn("authoriseUri");
 
         signInController.getSignIn(request, response);
@@ -60,12 +60,14 @@ public class SignInControllerTest {
     @Test
     void redirectForAuthTest() throws IOException {
         when(request.getRequestURL()).thenReturn(requestUrlStringBuffer);
-        when(oauth.oauth2EncodeState(any(String.class), any(String.class), any(String.class))).thenReturn("state");
+        when(oauth.oauth2EncodeState(any(String.class), any(String.class), any(String.class)))
+                .thenReturn("state");
         when(identityProvider.getAuthorisationUrl("state")).thenReturn("authoriseUri");
 
         signInController.redirectForAuth(session, request, response);
 
-        verify(oauth, times(1)).oauth2EncodeState(any(String.class), any(String.class), any(String.class));
+        verify(oauth, times(1))
+                .oauth2EncodeState(any(String.class), any(String.class), any(String.class));
         verify(identityProvider, times(1)).getAuthorisationUrl(any(String.class));
         verify(response, times(1)).sendRedirect(any(String.class));
 
@@ -73,9 +75,13 @@ public class SignInControllerTest {
 
     @Test
     void createAuthoriseURIWithForceAndHintTest() {
-        when(oauth.oauth2EncodeState(any(String.class), any(String.class), any(String.class))).thenReturn("hint");
-        when(identityProvider.getAuthorisationUrl(any(String.class), any(String.class))).thenReturn("authUrl");
+        when(oauth.oauth2EncodeState(any(String.class), any(String.class), any(String.class)))
+                .thenReturn("hint");
+        when(identityProvider.getAuthorisationUrl(any(String.class), any(String.class)))
+                .thenReturn("authUrl");
 
-        Assert.assertEquals(signInController.createAuthoriseURIWithForceAndHint(originalRequestUri, scope, nonce, email), "authUrl&reauthenticate=force&hint=hint");
+        Assert.assertEquals(signInController
+                        .createAuthoriseURIWithForceAndHint(originalRequestUri, scope, nonce, email),
+                "authUrl&reauthenticate=force&hint=hint");
     }
 }
