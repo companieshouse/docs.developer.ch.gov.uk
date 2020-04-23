fde05c2cef9c17c4150c28fa7da1d4259513afb6
Ngriffiths1 <ngriffiths@companieshouse.gov.uk>
2020-04-23 14:09:05 +0100

    fix merge conflict in SessionFactory

--- src/main/java/uk/gov/ch/oauth/session/SessionFactory.java
+++ src/main/java/uk/gov/ch/oauth/session/SessionFactory.java
@@ -1,18 +1,12 @@
 package uk.gov.ch.oauth.session;
 
 import java.util.Map;
-<<<<<<< HEAD:src/main/java/uk/gov/ch/developer/docs/session/SessionFactory.java
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.session.Session;
 import uk.gov.companieshouse.session.SessionImpl;
-=======
-import org.springframework.stereotype.Component;
-import uk.gov.companieshouse.session.Session;
 import uk.gov.companieshouse.session.handler.SessionHandler;
->>>>>>> origin/feature/user-oauth/ci:src/main/java/uk/gov/ch/oauth/session/SessionFactory.java
 import uk.gov.companieshouse.session.store.Store;
-
 
 /**
  * This is a wrapper component for {@link uk.gov.companieshouse.session.SessionFactory} that allows
