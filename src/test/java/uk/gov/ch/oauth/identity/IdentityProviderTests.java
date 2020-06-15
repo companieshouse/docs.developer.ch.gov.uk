331c470687ced17d84e304a0c9da880b58df93f5
ICraig-CH <icraig@companieshouse.gov.uk>
2020-06-15 10:48:13 +0100

    Changed the static final variables to be all in capital letters as I forgot to do it before.

--- src/test/java/uk/gov/ch/oauth/identity/IdentityProviderTests.java
+++ src/test/java/uk/gov/ch/oauth/identity/IdentityProviderTests.java
@@ -16,9 +16,9 @@ public class IdentityProviderTests {
     private IdentityProvider provider;
 
     private EnvironmentReaderImpl reader;
-    private static final String code = "code";
-    private static final String state = "state";
-    private static final String scope = "scope";
+    private static final String CODE = "code";
+    private static final String STATE = "state";
+    private static final String SCOPE = "scope";
 
     @BeforeEach
     public void init() {
@@ -31,7 +31,7 @@ public class IdentityProviderTests {
     @DisplayName("Test that getAuthorisationUrl(final String State) returns the expected result")
     public void testThatGetAuthorisationUrlWithStateParameterReturnsTheExpectedUrl() {
         String expectedAuthUrl = "/oauth2/authorise?client_id=test.apps.ch.gov.test&redirect_uri=/oauth2/user/callback&response_type=code&state=state";
-        String authUrl = provider.getAuthorisationUrl(state);
+        String authUrl = provider.getAuthorisationUrl(STATE);
         assertEquals(expectedAuthUrl, authUrl);
     }
 
@@ -39,14 +39,14 @@ public class IdentityProviderTests {
     @DisplayName("Test that getAuthorisationUrl(final String State, final String scope) returns the expected result")
     public void testThatGetAuthorisationWithScopeAndStateParameterReturnsTheExpectedURL() {
         String expectedUrl = "/oauth2/authorise?client_id=test.apps.ch.gov.test&redirect_uri=/oauth2/user/callback&response_type=code&state=state&scope=scope";
-        String authUrl = provider.getAuthorisationUrl(state, scope);
+        String authUrl = provider.getAuthorisationUrl(STATE, SCOPE);
         assertEquals(expectedUrl, authUrl);
     }
 
     @Test
     public void testThatGetPostRequestBodyReturnsTheExpectedResult() {
         String expectedRequestBody = "code=code&client_id=test.apps.ch.gov.test&client_secret=CLIwYzRkNzIwOGQ1OGQ0OWIzMzViYjJjOTEyYTc2&redirect_uri=/oauth2/user/callback&grant_type=authorization_code";
-        String actualRequestBody = provider.getPostRequestBody(code);
+        String actualRequestBody = provider.getPostRequestBody(CODE);
         assertEquals(expectedRequestBody, actualRequestBody);
     }
 }
