a5c733f78b57f67daacc997513eb803c293817a6
Jonathan Blake <jblake2@companieshouse.gov.uk>
2020-06-22 13:08:14 +0100

    Removed unsused code

--- src/test/java/uk/gov/ch/developer/docs/controller/developer/UserCallbackControllerTest.java
+++ src/test/java/uk/gov/ch/developer/docs/controller/developer/UserCallbackControllerTest.java
@@ -56,48 +56,4 @@ public class UserCallbackControllerTest {
         verify(mockCoordinator).getPostCallbackRedirectURL(mockResponse, mockParams);
         verifyNoMoreInteractions(mockCoordinator, mockResponse, mockParams);
     }
-
-//    @Test
-//    @DisplayName("Checking result when state or code are invalid")
-//    public void testGetCallbackInvalidCodeState() throws IOException {
-//
-//        doReturn(false).when(oauth2).validate(STATE, CODE, servletResponse);
-//
-////        userCallbackController
-////                .getCallback(STATE, CODE, servletResponse);
-//        verify(servletResponse).sendError(HttpServletResponse.SC_FORBIDDEN);
-//        verifyNoMoreInteractions(servletResponse);
-//    }
-//
-//    @Test
-//    @DisplayName("Checking result when state or code are invalid")
-//    public void testGetCallbackException() {
-//
-//        doThrow(new RuntimeException("bad connection")).when(oauth2)
-//                .validate(STATE, CODE, servletResponse);
-//
-////        userCallbackController.getCallback(STATE, CODE, servletResponse);
-//        verify(servletResponse).setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-//        verifyNoMoreInteractions(servletResponse);
-//    }
-//
-//    @Test
-//    @DisplayName("Checking result when state or code are valid")
-//    public void testGetCallbackSuccess() throws IOException {
-//        when(identityProvider.getRedirectUriPage()).thenReturn(HTTP_EXAMPLE_COM_REDIRECT);
-//
-//        doReturn(true).when(oauth2).validate(STATE, CODE, servletResponse);
-//
-////        userCallbackController
-////                .getCallback(STATE, CODE, servletResponse);
-//        verify(servletResponse).sendRedirect(HTTP_EXAMPLE_COM_REDIRECT);
-//        verifyNoMoreInteractions(servletResponse);
-//    }
-//
-//    @Test
-//    @DisplayName("Checking access refused sends forbidden error with reason.")
-//    public void accessRefused_Escalates403Error_test() throws IOException {
-////        userCallbackController.accessRefused(ERROR, mockResponse);
-//        verify(mockResponse, only()).sendError(403, ERROR);
-//    }
 }
