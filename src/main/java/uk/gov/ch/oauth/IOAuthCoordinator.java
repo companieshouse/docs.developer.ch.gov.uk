7cbc5389eb7c5d6748184873e7257a9fb3b09766
ICraig-CH <icraig@companieshouse.gov.uk>
2020-06-24 15:06:12 +0100

    Added Javadoc comments for getPostCallbackRedirectUrl() and added more detail to the javadocs for invalidateSession and getSignoutUri.

--- src/main/java/uk/gov/ch/oauth/IOAuthCoordinator.java
+++ src/main/java/uk/gov/ch/oauth/IOAuthCoordinator.java
@@ -8,16 +8,29 @@ import java.util.Map;
 
 public interface IOAuthCoordinator {
 
+    /**
+     * Will return the post-callback redirect Url so long as there are no errors in the params parameter,
+     * else an UnauthorisedException is thrown.
+     *
+     * @param response The HttpServletResponse.
+     * @param params   A Map of Strings each with a String key.
+     * @return A String that represents a Url
+     * @throws UnauthorisedException Will be thrown if there is an 'error' String in the params Map.
+     */
     String getPostCallbackRedirectURL(HttpServletResponse response, Map<String, String> params)
             throws UnauthorisedException;
 
     /**
+     * Calls the getRedirectUriPage method from the oauth library.
+     *
      * @return The redirect Uri so that it can be assigned to a parameter for the sendRedirect() method
      * of HttpServletResponse.
      */
     String getSignoutUri();
 
     /**
+     * Calls the invalidateSession method from the oauth library.
+     *
      * @param chSession The session that will be passed in so that the invalidateSession(Session session)
      *                  from the Oauth2 class can be called.
      */
