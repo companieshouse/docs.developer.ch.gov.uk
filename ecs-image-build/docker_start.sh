9dc6aedd023e11a28771aa8a3fed27cdf272f04e
Toba Folaranmi <ofolaranmi@companieshouse.gov.uk>
2023-06-01 09:19:37 +0100

    removing unsued args

--- ecs-image-build/docker_start.sh
+++ ecs-image-build/docker_start.sh
@@ -5,4 +5,3 @@
 PORT=8080
 
 exec java -jar -Dserver.port="${PORT}" "docs.developer.ch.gov.uk.jar"
-#exec java ${JAVA_MEM_ARGS} -jar -Dserver.port="${PORT}" -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 "${APP_DIR}/docs.developer.ch.gov.uk.jar"
