7b1802b6f088ef0d276f10665a6da45f3b52af1c
bcullerton <bcullerton@companieshouse.gov.uk>
2023-06-08 08:58:29 +0100

    replace antMatcher with securityMatcher to keep using pattern

--- src/main/java/uk/gov/ch/developer/docs/security/WebSecurity.java
+++ src/main/java/uk/gov/ch/developer/docs/security/WebSecurity.java
@@ -1,24 +1,19 @@
 package uk.gov.ch.developer.docs.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import uk.gov.companieshouse.session.handler.SessionHandler;
 
-@EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
+@Configuration
+public class WebSecurity {
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+        http.securityMatcher("/**").addFilterBefore(new SessionHandler(),
+                BasicAuthenticationFilter.class);
 
-    @Configuration
-    @Order(1)
-    public static class RootSecurityConfig extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.antMatcher("/**").addFilterBefore(new SessionHandler(),
-                    BasicAuthenticationFilter.class);
-        }
+        return http.build();
     }
-
 }
