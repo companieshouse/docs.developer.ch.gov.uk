830b9eb403b51bd9edf15bd026f881ca1a52265f
Harish Marimuthu <hmarimuthu@companieshouse.gov.uk>
2023-11-20 11:38:00 +0000

    CC-667 Adding healthcheck_path and healthcheck_matcher

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -33,8 +33,6 @@ module "ecs-service" {
   lb_listener_arn                   = data.aws_lb_listener.dev_site_lb_listener.arn
   lb_listener_rule_priority         = local.lb_listener_rule_priority
   lb_listener_paths                 = local.lb_listener_paths
-  healthcheck_path          = local.healthcheck_path
-  healthcheck_matcher       = local.healthcheck_matcher
   health_check_grace_period_seconds = 240
   healthcheck_healthy_threshold     = "2"
 
@@ -43,6 +41,8 @@ module "ecs-service" {
   docker_repo       = local.docker_repo
   container_version = var.docs_developer_version
   container_port    = local.container_port
+  healthcheck_path          = local.healthcheck_path
+  healthcheck_matcher       = local.healthcheck_matcher
 
   # Service configuration
   service_name = local.service_name
