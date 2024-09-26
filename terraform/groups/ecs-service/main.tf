3f36275b26adbb5896c1829e5343c846524024b8
NeilShadrach <neil.shadrach@corryn.com>
2024-09-26 11:35:00 +0100

    Reapply "DVOP-3031 Bumping the module version"
    This reverts commit f17051592f3e949251a3d8755189e3e7702e664a.

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -18,7 +18,7 @@ terraform {
 }
 
 module "ecs-service" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.258"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.289"
 
   # Environmental configuration
   environment             = var.environment
