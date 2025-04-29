b5a395ef1c6f885f1e86843462f8128e35d4e51b
johncookch <jcook@companieshouse.gov.uk>
2025-04-29 11:18:41 +0100

    CC-2119: Updated the Java session handler version
    * Bumped the version of the java session handler
    * Removed the old signout method that no longer exists in the Java session handler
    * Added a new signout redirect URL to the SignOutController

--- src/main/java/uk/gov/ch/developer/docs/controller/developer/SignOutController.java
+++ src/main/java/uk/gov/ch/developer/docs/controller/developer/SignOutController.java
@@ -1,16 +1,12 @@
 package uk.gov.ch.developer.docs.controller.developer;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import uk.gov.ch.developer.docs.DocsWebApplication;
-import uk.gov.ch.oauth.IOAuthCoordinator;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
-import uk.gov.companieshouse.session.Session;
-import uk.gov.companieshouse.session.handler.SessionHandler;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -19,20 +15,17 @@ public class SignOutController {
     private static final Logger LOGGER =
             LoggerFactory.getLogger(DocsWebApplication.APPLICATION_NAME_SPACE);
 
-    @Autowired
-    private IOAuthCoordinator ioAuthCoordinator;
+    private final String redirectUri;
 
-    @GetMapping("${signout.url}")
-    public void doSignOut(final HttpServletResponse httpServletResponse,
-                          final HttpServletRequest httpServletRequest) throws IOException {
-
-        LOGGER.debug("Processing sign out");
-
-        final var chSession = (Session) httpServletRequest
-                .getAttribute(SessionHandler.CHS_SESSION_REQUEST_ATT_KEY);
-        String redirectUri = chSession.signOut(httpServletRequest);
-        ioAuthCoordinator.getOAuth().invalidateSession(chSession);
-        httpServletResponse.sendRedirect(redirectUri);
+    public SignOutController(@Value("signout.redirect.url") String redirectUri) {
+        this.redirectUri = redirectUri;
     }
 
+    @GetMapping("${signout.url}")
+    public void doSignOut(final HttpServletResponse httpServletResponse) throws IOException {
+
+        LOGGER.debug("Processing sign out. Redirecting to " + redirectUri);
+
+        httpServletResponse.sendRedirect(redirectUri);
+    }
 }
--- src/test/java/uk/gov/ch/developer/docs/controller/developer/SignOutControllerTest.java
+++ src/test/java/uk/gov/ch/developer/docs/controller/developer/SignOutControllerTest.java
@@ -3,20 +3,13 @@ package uk.gov.ch.developer.docs.controller.developer;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import uk.gov.ch.oauth.IOAuthCoordinator;
-import uk.gov.ch.oauth.IOauth;
-import uk.gov.companieshouse.session.Session;
-import uk.gov.companieshouse.session.handler.SessionHandler;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class SignOutControllerTest {
@@ -24,41 +17,15 @@ class SignOutControllerTest {
     private static final String REDIRECT_PAGE = "/home";
 
     @Mock
-    private IOAuthCoordinator ioAuthCoordinator;
-    @Mock
-    private IOauth iOauth;
-    @Mock
-    private HttpServletRequest httpServletRequest;
-    @Mock
     private HttpServletResponse httpServletResponse;
-    @Mock
-    private Session session;
-    @InjectMocks
-    private SignOutController signOutController;
 
-    @Test
-    @DisplayName("Test that invalidate Session Is Called when signing out")
-    void testThatInvalidateSessionIsCalled() throws IOException {
-        when(httpServletRequest.getAttribute(SessionHandler.CHS_SESSION_REQUEST_ATT_KEY))
-                .thenReturn(session);
-        when(ioAuthCoordinator.getOAuth()).thenReturn(iOauth);
-
-        signOutController.doSignOut(httpServletResponse, httpServletRequest);
-
-        verify(iOauth).invalidateSession(session);
-    }
+    private final SignOutController signOutController = new SignOutController(REDIRECT_PAGE);
 
     @Test
     @DisplayName("Test that a user is redirected upon signing out")
     void testDoSignOut() throws IOException {
-        when(httpServletRequest.getAttribute(SessionHandler.CHS_SESSION_REQUEST_ATT_KEY))
-                .thenReturn(session);
-        when(session.signOut(httpServletRequest)).thenReturn(REDIRECT_PAGE);
-        when(ioAuthCoordinator.getOAuth()).thenReturn(iOauth);
-
-        signOutController.doSignOut(httpServletResponse, httpServletRequest);
+        signOutController.doSignOut(httpServletResponse);
 
         verify(httpServletResponse).sendRedirect(REDIRECT_PAGE);
     }
-
 }
