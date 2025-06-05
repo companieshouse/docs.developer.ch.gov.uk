b0d8f52cb6efc49627783666ed7d8e003e4cd9d7
John Griffiths <74862880+griffithsjd@users.noreply.github.com>
2025-06-05 19:05:31 +0100

    Revert "FCS-41 align terraform with oauth-web"

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -18,6 +18,7 @@ locals {
   use_set_environment_files   = var.use_set_environment_files
   application_subnet_ids      = data.aws_subnets.application.ids
   application_subnet_pattern = local.stack_secrets["application_subnet_pattern"]
+
   stack_secrets              = jsondecode(data.vault_generic_secret.stack_secrets.data_json)
   service_secrets            = jsondecode(data.vault_generic_secret.service_secrets.data_json)
 
@@ -28,18 +29,24 @@ locals {
     trimprefix(sec.name, "/${local.name_prefix}/") => sec.arn
   }
 
-  service_secrets_arn_map = {
-    for sec in module.secrets.secrets:
-    trimprefix(sec.name, "/${local.service_name}-${var.environment}/") => sec.arn
+  global_secrets_arn_map = {
+    for sec in data.aws_ssm_parameter.global_secret :
+    trimprefix(sec.name, "/${local.global_prefix}/") => sec.arn
   }
 
   global_secret_list = flatten([for key, value in local.global_secrets_arn_map :
     { "name" = upper(key), "valueFrom" = value }
   ])
 
-  global_secrets_arn_map = {
-    for sec in data.aws_ssm_parameter.global_secret :
-    trimprefix(sec.name, "/${local.global_prefix}/") => sec.arn
+  ssm_global_version_map = [
+    for sec in data.aws_ssm_parameter.global_secret : {
+      name = "GLOBAL_${var.ssm_version_prefix}${replace(upper(basename(sec.name)), "-", "_")}", value = sec.version
+    }
+  ]
+
+  service_secrets_arn_map = {
+    for sec in module.secrets.secrets:
+    trimprefix(sec.name, "/${local.service_name}-${var.environment}/") => sec.arn
   }
 
   service_secret_list = flatten([for key, value in local.service_secrets_arn_map :
@@ -52,15 +59,14 @@ locals {
     }
   ]
 
-  ssm_global_version_map = [
-    for sec in data.aws_ssm_parameter.global_secret : {
-      name = "GLOBAL_${var.ssm_version_prefix}${replace(upper(basename(sec.name)), "-", "_")}", value = sec.version
-    }
-  ]
-
   # docs.developer secrets to go in list
-  task_secrets = concat(local.global_secret_list,local.service_secret_list)
+  task_secrets = concat(local.global_secret_list,local.service_secret_list,[
+    { "name" : "CHS_DEVELOPER_CLIENT_ID", "valueFrom" : local.global_secrets_arn_map.oauth2_client_id },
+    { "name" : "CHS_DEVELOPER_CLIENT_SECRET", "valueFrom" : local.global_secrets_arn_map.oauth2_client_secret },
+    { "name" : "DEVELOPER_OAUTH2_REQUEST_KEY", "valueFrom" : local.global_secrets_arn_map.oauth2_request_key }
+  ])
 
-  task_environment = concat(local.ssm_global_version_map,local.ssm_service_version_map)
-
+  task_environment = concat(local.ssm_global_version_map,local.ssm_service_version_map,[
+    { "name" : "DOC_DEVELOPER_SERVICE_PORT", "value" : local.container_port }
+  ])
 }
