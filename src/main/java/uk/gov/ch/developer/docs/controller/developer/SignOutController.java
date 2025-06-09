c7dcf62dc428b5a5b2fd1962aaea7d02a64d4885
John Griffiths <jgriffiths2@companieshouse.gov.uk>
2025-06-09 10:13:44 +0100

    FCS-41 map signin/out endpoints correctly

--- src/main/java/uk/gov/ch/developer/docs/controller/developer/SignOutController.java
+++ src/main/java/uk/gov/ch/developer/docs/controller/developer/SignOutController.java
@@ -3,6 +3,7 @@ package uk.gov.ch.developer.docs.controller.developer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import uk.gov.companieshouse.logging.Logger;
 
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,6 +14,7 @@ import java.io.IOException;
 import static uk.gov.ch.developer.docs.DocsWebApplication.APPLICATION_NAME_SPACE;
 
 @Controller
+@RequestMapping("${signout.url}")
 public class SignOutController {
 
     @Value("${signout.redirect.url}")
@@ -20,7 +22,7 @@ public class SignOutController {
 
     private static final Logger logger = LoggerFactory.getLogger(APPLICATION_NAME_SPACE);
 
-    @GetMapping("${signout.url}")
+    @GetMapping
     public void doSignOut(final HttpServletResponse httpServletResponse) throws IOException {
 
         logger.debug("Processing sign out. Redirecting to " + redirectUri);
