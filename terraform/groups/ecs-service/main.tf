a5f3e5888350e165b87a608fc84e690b41db9830
garethpowell-ch <gpowell1@companieshouse.gov.uk>
2026-08-24 16:09:20 +0100

    ASM-1251 Update ECS service and secrets module source references to version 1.0.413

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -18,7 +18,7 @@ terraform {
 }
 
 module "ecs-service" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.407"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.413"
 
   # Environmental configuration
   environment             = var.environment
@@ -74,10 +74,9 @@ module "ecs-service" {
 }
 
 module "secrets" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.294"
+  source = "git@github.com:companieshouse/terraform-modules//aws/parameter-store?ref=1.0.413"
 
   name_prefix = "${local.service_name}-${var.environment}"
-  environment = var.environment
   kms_key_id  = data.aws_kms_key.kms_key.id
   secrets     = nonsensitive(local.service_secrets)
 }
